56331a78f191d81f119c68beffc212952ea24238
Kevin Benevides <39235256801@info.local>
2018-09-27 08:08:39 -0300

    atualizando

---
+++ Senai.Operadores.Exercicio5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Senai.Operadores.Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o preço do produto: ");
+            float produto = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade mínima: ");
+            sbyte qmin = Convert.ToSByte(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade máxima: ");
+            sbyte qmax = Convert.ToSByte(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade atual do estoque: ");
+            sbyte qat = Convert.ToSByte(Console.ReadLine()); 
+
+            int em = (qmin + qmax) /2 ;
+            float custoem = produto*em;
+            float custoet = produto*qat;
+
+            Console.WriteLine($"Estoque Médio= {em}");
+            Console.WriteLine($"Custo total do Estoque Médio= {custoem}");
+            Console.WriteLine($"Custo total do Estoque Atual= {custoet}");
+        }
+    }
+}
---
+++ Senai.Operadores.Exercicio6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Senai.Operadores.Exercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do produto: ");
+            string nomeproduto = Console.ReadLine();
+
+            Console.WriteLine("Digite o preço do produto: ");
+            ushort precoproduto = ushort.Parse(Console.ReadLine());
+
+            float porcento = (precoproduto * 10) /100;
+
+            float desconto = (precoproduto - porcento) ;
+
+            Console.WriteLine($"Preço= {precoproduto}");
+            Console.WriteLine($"Preço com 10% de desconto= {desconto}");
+        }
+    }
+}
